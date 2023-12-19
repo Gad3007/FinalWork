c044274a14cbe524503bd3eddb329b7147633259
Ageev Ilya <Aii23@mail.ru>
2023-12-19 22:46:07 +0300

    Создал цикл выбора элементов массива с 3-мя знаками

--- Program.cs
+++ Program.cs
@@ -2,5 +2,13 @@
 
 int len_array = array.Length;
 
+for (int i = 0; i < len_array; i++)
+{
 
+    string str_Val = array[i];
+    if (str_Val.Length <= 3)
+    {
+        Console.Write( str_Val + ", ");
+    }
+}
 
